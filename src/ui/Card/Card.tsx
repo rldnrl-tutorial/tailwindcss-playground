2fa386560a18651dae0da247f7b26e915675da1b
rldnrl <devpark1002@gmail.com>
2022-05-11 21:43:35 +0900

    feat: next-image로 변경

--- src/ui/Card/Card.tsx
+++ src/ui/Card/Card.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
+import Image from "next/image";
 import React from "react";
 
 interface CardProps {
@@ -12,7 +13,9 @@ export default function Card({ title, description, src, alt }: CardProps) {
   return (
     <div className="mt-8">
       <div className="bg-white rounded-md overflow-hidden shadow">
-        <img src={src} alt={alt} className="w-full h-32 sm:h-48 object-cover" />
+        <div className="w-full h-32 relative">
+          <Image src={src} alt={alt} layout="fill" objectFit="cover" />
+        </div>
         <div>
           <span className="font-bold">{title}</span>
           <span className="block text-gray-500 text-sm">{description}</span>
