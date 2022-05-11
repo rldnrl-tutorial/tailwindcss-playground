6ec9423cea87265afe800e631fb7eb7b8ccfe4cd
rldnrl <devpark1002@gmail.com>
2022-05-11 21:36:55 +0900

    feat: card 컴포넌트 구현

---
+++ src/ui/Card/Card.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import React from "react";
+
+interface CardProps {
+  title?: string;
+  description?: string;
+  src: string;
+  alt: string;
+}
+
+export default function Card({ title, description, src, alt }: CardProps) {
+  return (
+    <div className="mt-8">
+      <div className="bg-white rounded-md overflow-hidden shadow">
+        <img src={src} alt={alt} className="w-full h-32 sm:h-48 object-cover" />
+        <div>
+          <span className="font-bold">{title}</span>
+          <span className="block text-gray-500 text-sm">{description}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
