44212c36262352cdce758a5c292232928ae86c56
rldnrl <devpark1002@gmail.com>
2022-05-11 20:16:20 +0900

    feat: layout 컴포넌트 구현

---
+++ src/layouts/Layout.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import { ReactNode } from "react";
+
+interface LayoutProps {
+  children?: ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <>
+      <div>
+        <nav>
+          <div>
+            <h1>
+              <Link href="/">Food</Link>
+            </h1>
+          </div>
+          <ul>
+            <li>
+              <Link href="#">
+                <span>Home</span>
+              </Link>
+            </li>
+            <li>
+              <Link href="#">
+                <span>About</span>
+              </Link>
+            </li>
+            <li>
+              <Link href="#">
+                <span>Contact</span>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      {children}
+    </>
+  );
+}
